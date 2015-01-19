ca2d2cce2b33e4c1c46d7e9e4d2700b2b9547594
Javier G. Sogo <jgsogo+github@gmail.com>
2015-01-19 12:54:32 +0100

    se añaden los edges con pesos

--- main.cpp
+++ main.cpp
@@ -13,8 +13,6 @@
 
 #include "jgsogo/AnCO/config.h"
 
-#include "jgsogo/AnCO/algorithm/aco_base.h"
-#include "jgsogo/AnCO/algorithm/prox_base.h"
 #include "jgsogo/AnCO/graph/memgraph.h"
 #include "jgsogo/AnCO/graph/graph_data_file.h"
 #include "jgsogo/AnCO/log.h"
@@ -22,6 +20,7 @@
 
 #include "jgsogo/AnCO/algorithm/aco_base.h"
 #include "jgsogo/AnCO/algorithm/aco_random.h"
+#include "jgsogo/AnCO/algorithm/aco_mmas.h"
 #include "jgsogo/AnCO/algorithm/prox_base.h"
 #include "jgsogo/AnCO/algorithm/prox1.h"
 #include "jgsogo/AnCO/algorithm/prox_percent.h"
@@ -53,22 +52,30 @@ typedef AnCO::neighbourhood<aco_algorithm, prox_algorithm> neighbourhood_type;
 struct success_meta : success_node_found {
     success_meta(_t_graph::_t_node_id id) : success_node_found(id) {};
     success_meta(success_meta& other) : success_node_found(other.id) {};
+    virtual void new_ant() { tmp.clear();};
     virtual bool operator()(edge_ptr ptr) {
-        tmp.push_back(ptr->end);
+        tmp.push_back(ptr);
         bool ret = (ptr->end == id);
-        std::cout << ".";
         if (ret) {
-            std::cout << "found! ";
-            for (auto i =0; i<tmp.size(); ++i) {
-                std::cout << tmp[i] << " -> ";
-                }
-            std::cout << std::endl;
-            //success_meta::v.push_back(tmp);
+            add_to_succesful(tmp);
             }
         return ret;
         };
-    std::vector<std::string> tmp;
-    //static std::vector<std::vector<std::string>> v;
+
+    void add_to_succesful(std::vector<edge_ptr>& path) {
+        // compute hash
+        std::string hash = std::accumulate(path.begin(), path.end(), (*path.begin())->init, [](std::string x, edge_ptr ptr){ return x+ptr->end;});
+        std::set<std::string>::iterator it; bool inserted;
+        std::tie(it, inserted) = hash_paths.insert(hash);
+        if (inserted) {
+            succesful_paths.push_back(path);
+            }
+        };
+
+    std::vector<edge_ptr> tmp;
+    
+    std::set<std::string> hash_paths;
+    std::vector<std::vector<edge_ptr>> succesful_paths;
     };
 
 int main(int argc, char* argv[]) {
@@ -82,12 +89,11 @@ int main(int argc, char* argv[]) {
         cfg.dataset = argv[2];
         }
 
-
     #ifdef _WINDOWS
         HWND console = GetConsoleWindow();
         RECT r;
         GetWindowRect(console, &r); //stores the console's current dimensions
-        MoveWindow(console, r.left, r.top, 1200, 800, TRUE); // 800 width, 100 height
+        MoveWindow(console, r.left, r.top, 1500, 800, TRUE); // 800 width, 100 height
     #endif
 
     std::cout << "======" << std::endl;
@@ -105,7 +111,7 @@ int main(int argc, char* argv[]) {
     AnCO::memgraph graph(dataset);
     t.toc();
 
-    std::cout << "3) Create neighbourhood of '" << cfg.n_colonies << "' colonies" << std::endl;
+    std::cout << "3) Create neighbourhood of '" << cfg.n_colonies << "' colonies (aco_random)" << std::endl;
     neighbourhood_type colony_meta(graph, cfg.n_colonies, cfg.n_ants_per_colony, cfg.max_steps);
 
     unsigned int max_i = 30;
@@ -160,28 +166,34 @@ int main(int argc, char* argv[]) {
         }
     
     if (start_colony.get_metric()<=0.f || end_colony.get_metric()<=0.f) {
-        std::cout << " NO path found!!" << std::endl;
+        std::cout << " NO path possible (?)!!" << std::endl;
         return 1;
         }
 
+    std::cout << "------------------------" << std::endl << std::endl;
     std::cout << "6) Build metagraph" << std::endl;
     AnCO::graph_data_file_builder meta_dataset;
     // nodos
+    std::cout << "\t - nodes: base node of each colony" << std::endl;
     meta_dataset.add_node(start_node->id);
     meta_dataset.add_node(end_node->id);
     auto colonies = colony_meta.get_colonies();
     std::for_each(colonies.begin(), colonies.end(), [&meta_dataset](const std::shared_ptr<colony_neighbourhood_type>& item){
         meta_dataset.add_node(item->get_base_node());
         });
+
     // edges
+    std::cout << "\t - edges: neighbourhood with 'probability > 0.f', cost for 'edge = 1-probability'" << std::endl;
     auto prox_start = start_colony.get_proximity_vector();
     auto prox_end = end_colony.get_proximity_vector();
     for (auto i=0; i<colonies.size(); i++) {
         if (prox_start[i]>0.f) {
-            meta_dataset.add_edge(start_node->id, colonies[i]->get_base_node());
+            meta_dataset.add_edge(start_node->id, colonies[i]->get_base_node(), 1-prox_start[i]);
+            std::cout << "\t\t " << start_node->id << " -> " << colonies[i]->get_base_node() << " | cost= '" << 1-prox_start[i] << "'" << std::endl;
             }
         if (prox_end[i]>0.f) {
-            meta_dataset.add_edge(colonies[i]->get_base_node(), end_node->id);
+            meta_dataset.add_edge(colonies[i]->get_base_node(), end_node->id, 1-prox_end[i]);
+            std::cout << "\t\t " << colonies[i]->get_base_node() << " -> " << end_node->id << " | cost= '" << 1-prox_end[i] << "' <-- :S no garantizado!!" << std::endl;
             }
         }
 
@@ -189,14 +201,15 @@ int main(int argc, char* argv[]) {
     for (auto i=0; i<colonies.size(); i++) {
         for (auto j=0; j<colonies.size(); j++) {
             if ((i!=j) && (matrix[i][j]>0.f)) {
-                meta_dataset.add_edge(colonies[i]->get_base_node(), colonies[j]->get_base_node());
+                meta_dataset.add_edge(colonies[i]->get_base_node(), colonies[j]->get_base_node(), 1 - matrix[i][j]);
+                std::cout << "\t\t " << colonies[i]->get_base_node() << " -> " << colonies[j]->get_base_node() << " | cost= '" << 1 - matrix[i][j] << "'" << std::endl;
                 }
             }
         }
     memgraph meta_graph(meta_dataset);
     
-    std::cout << "7) Search for path in metagraph" << std::endl;
-    colony_type search_colony(meta_graph, cfg.n_ants_per_colony, cfg.max_steps);
+    std::cout << std::endl << "7) Search for meta-path in meta-graph (MMAS)" << std::endl;
+    AnCO::colony<algorithm::aco_mmas> search_colony(meta_graph, cfg.n_ants_per_colony, cfg.max_steps);
     search_colony.set_base_node(start_node->id);
     iterations = 0;
 
@@ -205,19 +218,23 @@ int main(int argc, char* argv[]) {
     while (++iterations < max_i) {
         search_colony.run(success);
         search_colony.update();
-        colony_type::aco_algorithm_impl::update_graph(meta_graph);
-        /*
-        auto paths = search_colony.get_paths();
-        for (auto p = paths.begin(); p!=paths.end(); p++) {
-            std::cout << " - ant ";
-            for (auto i = (*p).begin(); i!=(*p).end(); i++) {
-                std::cout << (*i)->end << " -> " << std::endl;
+        colony_type::aco_algorithm_impl::update_graph(meta_graph);        
+        }
+    if (success.succesful_paths.size()) {
+        // Select path with
+        std::cout << "\t - candidate meta-paths" << std::endl;
+        std::cout << "\t\t cost | path" << std::endl;
+        for (auto it = success.succesful_paths.begin(); it!=success.succesful_paths.end(); ++it) {
+            float cost = std::accumulate(it->begin(), it->end(), 0.f, [](float x, edge_ptr ptr){ return x + ptr->data.length;});
+            std::cout << "\t\t" << cost << " | " << (*it->begin())->init;
+            for (auto jj = it->begin(); jj!=it->end(); ++jj) {
+                std::cout << " -> " << (*jj)->end;
                 }
             std::cout << std::endl;
             }
-        */
         }
 
     std::cout << "Done" << std::endl;
+    getchar();
     return 0;
     }
